8028c54f777be272ebb375f5732ae063469fecbe
Christopher Brown <cbrown@pivotal.io>
2018-08-10 11:02:32 -0700

    cgroup: improve cgroup mounting sophistication
    Not all kernels will have all of the cgroups enabled so instead of
    hard-coding a list of cgroups we should query the kernel to see which
    groups are actually available.
    
    Additionally, if we ourselves are already running in a cgroup then we
    aren't able to mount different cgroups in different hierarchies to the
    parent[1]. Therefore we must read our own cgroup groups and match those
    with our own mounts.
    
    [1]: https://lwn.net/Articles/606699/

--- src/bpm/cgroups/cgroup.go
+++ src/bpm/cgroups/cgroup.go
@@ -16,41 +16,51 @@
 package cgroups
 
 import (
+	"bufio"
+	"fmt"
+	"io"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"golang.org/x/sys/unix"
 
 	"bpm/mount"
 )
 
-const cgroupFilesystem = "cgroup"
-
-var (
-	cgroupRoot       = filepath.Join("/sys", "fs", "cgroup")
-	legacyCgroupRoot = filepath.Join("/cgroup", "bpm")
-	subsystems       = []string{"blkio", "cpu", "cpuacct", "cpuset", "devices", "freezer", "hugetlb", "memory", "perf_event", "pids"}
+const (
+	cgroupRoot       = "/sys/fs/cgroup"
+	legacyCgroupRoot = "/cgroup/bpm"
 )
+
+// Needed for legacy cgroups migration. Not used in new code.
+var subsystems = []string{"blkio", "cpu", "cpuacct", "cpuset", "devices", "freezer", "hugetlb", "memory", "perf_event", "pids"}
 
 func Setup() error {
 	mnts, err := mount.Mounts()
 	if err != nil {
 		return err
 	}
+	if err := mountCgroupTmpfsIfNotPresent(mnts); err != nil {
+		return err
+	}
 
-	err = mountCgroupTmpfsIfNotPresent(mnts)
+	subsystems, err := EnabledSubsystems()
 	if err != nil {
 		return err
 	}
 
-	for _, subsystem := range subsystems {
-		err := mountCgroupSubsystemIfNotPresent(mnts, subsystem)
+	for _, sub := range subsystems {
+		group, err := SubsystemGrouping(sub)
 		if err != nil {
+			return err
+		}
+		if err := mountCgroupSubsystem(group); err != nil {
 			return err
 		}
 	}
 
-	// JM: This exists for the legacy code cgroup mount point
+	// TODO(jm): This exists for the legacy code cgroup mount point
 	// This should be deleted when v1 is released.
 	err = chmodLegacyMountPointIfPresent()
 	if err != nil {
@@ -58,6 +68,89 @@ func Setup() error {
 	}
 
 	return nil
+}
+
+// SubsystemGrouping fetches the parent cgroup grouping (if any) for a
+// particular subsystem.
+func SubsystemGrouping(subsystem string) (string, error) {
+	f, err := os.Open("/proc/self/cgroup")
+	if os.IsNotExist(err) {
+		// If the current process is not in a cgroup then we can do as we
+		// please. We do not mount cgroups together.
+		return subsystem, nil
+	}
+	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	// If the current process is in a cgroup then we need to match the
+	// grouping of the parent cgroup.
+	return subsystemGrouping(f, subsystem)
+}
+
+func subsystemGrouping(f io.Reader, subsystem string) (string, error) {
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		line := s.Text()
+		fields := strings.Split(line, ":")
+		grouping := fields[1]
+		subs := strings.Split(grouping, ",")
+		if containsElement(subs, subsystem) {
+			return grouping, nil
+		}
+	}
+	if err := s.Err(); err != nil {
+		return "", err
+	}
+
+	// If the current process isn't in a cgroup of the subsystem's type
+	// then we don't need to match anything.
+	return subsystem, nil
+}
+
+// EnabledSubsystems returns the cgroup subsystems which are enabled in the
+// running kernel.
+func EnabledSubsystems() ([]string, error) {
+	f, err := os.Open("/proc/cgroups")
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	return enabledSubsystems(f)
+}
+
+func enabledSubsystems(f io.Reader) ([]string, error) {
+	var subs []string
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		line := s.Text()
+
+		// Skip header
+		if strings.HasPrefix(line, "#") {
+			continue
+		}
+
+		var (
+			name    string
+			ignored int
+			enabled bool
+		)
+		if _, err := fmt.Sscanf(s.Text(), "%s %d %d %t", &name, &ignored, &ignored, &enabled); err != nil {
+			return nil, err
+		}
+		if enabled {
+			subs = append(subs, name)
+		}
+	}
+
+	if err := s.Err(); err != nil {
+		return nil, err
+	}
+
+	return subs, nil
 }
 
 func mountCgroupTmpfsIfNotPresent(mnts []mount.Mnt) error {
@@ -75,25 +168,23 @@ func mountCgroupTmpfsIfNotPresent(mnts []mount.Mnt) error {
 	return mount.Mount("tmpfs", cgroupRoot, "tmpfs", unix.MS_NOSUID|unix.MS_NOEXEC|unix.MS_NODEV, "mode=755")
 }
 
-func mountCgroupSubsystemIfNotPresent(mnts []mount.Mnt, subsystem string) error {
-	for _, mnt := range mnts {
-		if mnt.Filesystem == cgroupFilesystem && containsElement(mnt.Options, subsystem) {
-			return nil
-		}
-	}
-
+func mountCgroupSubsystem(subsystem string) error {
 	mountPoint := filepath.Join(cgroupRoot, subsystem)
-	err := os.MkdirAll(mountPoint, 0755)
-	if err != nil {
+	if err := os.MkdirAll(mountPoint, 0755); err != nil {
+		return err
+	}
+	if err := os.Chmod(mountPoint, 0755); err != nil {
 		return err
 	}
 
-	err = os.Chmod(mountPoint, 0755)
-	if err != nil {
+	err := mount.Mount("cgroup", mountPoint, "cgroup", 0, subsystem)
+	switch err {
+	// EBUSY is returned if the mountpoint already has something mounted on it
+	case unix.EBUSY, nil:
+		return nil
+	default:
 		return err
 	}
-
-	return mount.Mount(cgroupFilesystem, mountPoint, cgroupFilesystem, 0, subsystem)
 }
 
 func containsElement(elements []string, element string) bool {
