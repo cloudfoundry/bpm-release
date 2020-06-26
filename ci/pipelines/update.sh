fe64fe2f56e2c12e0b99196f45f0520458f51be6
Aakash Shah <ashah@pivotal.io>
2020-06-26 11:33:37 -0700

    Move BPM pipeline to BOSH concourse
    * Fix first-pass of errors. "start-bosh" must now be sourced instead of
    called.
    
    [#173518813]

--- ci/pipelines/update.sh
+++ ci/pipelines/update.sh
@@ -4,11 +4,12 @@ set -e
 set -u
 set -o pipefail
 
+# Shared-CF BPM/bpm-pipeline-creds
 NOTE_ID="4061975348382289614"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-fly -t bpm set-pipeline \
+fly -t bosh set-pipeline \
   -p bpm \
   -c "${SCRIPT_DIR}/bpm.yml" \
   --load-vars-from <(lpass show "$NOTE_ID" --notes)
