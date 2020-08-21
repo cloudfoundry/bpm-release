cce5602c9182e00aece8c8b42dfc433a6e31cc36
Kevin Rutten <krutten@vmware.com>
2020-08-20 20:59:36 -0700

    After moving BPMs pipeline, update the script (#147)
    [#173518813](https://www.pivotaltracker.com/story/show/173518813)

--- ci/pipelines/update.sh
+++ ci/pipelines/update.sh
@@ -10,7 +10,7 @@ NOTE_ID="4061975348382289614"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 fly -t bosh set-pipeline \
-  -p bpm \
+  -p bosh-team \
   -c "${SCRIPT_DIR}/bpm.yml" \
   --load-vars-from <(lpass show "$NOTE_ID" --notes)
 
