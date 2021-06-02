8fb94059dd8430e1cebeb47c7f6ef7f7c40a0c66
Joseph Palermo <jpalermo@pivotal.io>
2021-06-02 14:54:41 -0700

    Move ci pipeline to bosh-ecosystem concourse

--- ci/pipelines/configure.sh
+++ ci/pipelines/configure.sh
@@ -9,8 +9,7 @@ NOTE_ID="4061975348382289614"
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-fly -t bosh set-pipeline \
-  -p bosh-team \
+fly -t bosh-ecosystem set-pipeline \
+  -p bpm \
   -c "${SCRIPT_DIR}/bpm.yml" \
   --load-vars-from <(lpass show "$NOTE_ID" --notes)
-
