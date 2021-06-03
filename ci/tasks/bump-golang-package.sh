6473c5c24420bf45f3f02a82d3bb497bf3331c96
Joseph Palermo <jpalermo@pivotal.io>
2021-06-02 18:22:35 -0700

    Fixes to new bump golang package ci job

--- ci/tasks/bump-golang-package.sh
+++ ci/tasks/bump-golang-package.sh
@@ -9,7 +9,7 @@ git config --global user.name "CI Bot"
 
 cd bpm-release
 
-echo "${BOSH_PRIVATE_CONFIG}" > bumped-bosh-release/config/private.yml
+echo "${BOSH_PRIVATE_CONFIG}" > config/private.yml
 
 bosh vendor-package golang-1-linux "$task_dir/golang-release"
 
