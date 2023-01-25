4f389eafac32e7a5157efa86532f17de92a1536b
Joseph Palermo <jpalermo@pivotal.io>
2023-01-25 09:12:01 -0800

    Update ci pipeline with new credhub entries

--- ci/pipelines/configure.sh
+++ ci/pipelines/configure.sh
@@ -4,12 +4,8 @@ set -e
 set -u
 set -o pipefail
 
-# Shared-CF BPM/bpm-pipeline-creds
-NOTE_ID="4061975348382289614"
-
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 fly -t bosh-ecosystem set-pipeline \
   -p bpm \
-  -c "${SCRIPT_DIR}/bpm.yml" \
-  --load-vars-from <(lpass show "$NOTE_ID" --notes)
+  -c "${SCRIPT_DIR}/bpm.yml"
