5d34a012566b27ce16a58afb303afaac2281f342
aram price <aram.price@broadcom.com>
2024-03-14 17:17:59 -0700

    Compose `test-with-docker` with `start-docker` to reduce duplication
    - inline the contents of `checkdocker.sh`

--- scripts/checkdocker.sh
+++
@@ -1,7 +0,0 @@
-# The Docker CLI does not need to be run as root in order to be safe in all
-# cases. For example, when running on MacOS it is quarantined to a different
-# virtual machine. If you are running daemon on a local machine then practice
-# caution with sudo-less docker invocations.
-user_can_docker() {
-  ! docker info 2>&1 | grep -q "permission denied"
-}
