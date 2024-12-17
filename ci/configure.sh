7acec29398a3c9155aaf195f6547547623af3620
Aram Price <aram.price@broadcom.com>
2024-12-17 10:51:14 -0800

    Make ci directory consistent with other repos
    Also get rid of a xenial reference.
    
    Co-authored-by: Chris Selzo <chris.selzo@broadcom.com>

--- ci/configure.sh
+++ ci/configure.sh
@@ -8,4 +8,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 fly -t bosh-ecosystem set-pipeline \
   -p bpm \
-  -c "${SCRIPT_DIR}/bpm.yml"
+  -c "${SCRIPT_DIR}/pipeline.yml"
