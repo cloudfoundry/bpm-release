b089672822ea81b9ef1f35abe10ddcf62e6e0661
Rajath Agasthya <rajath.agasthya@broadcom.com>
2025-04-16 13:59:05 -0500

    Move bpm pipeline to OSS concourse
    Signed-off-by: Julian Hjortshoj <julian.hjortshoj@broadcom.com>

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-set -e
-set -u
-set -o pipefail
+set -euo pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-fly -t bosh-ecosystem set-pipeline \
+fly -t "${CONCOURSE_TARGET:-bosh}" set-pipeline \
   -p bpm \
   -c "${SCRIPT_DIR}/pipeline.yml"
